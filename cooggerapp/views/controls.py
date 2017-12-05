93e536ef467b3ba7cdbab979965e6e15e26e5bbf
hakan celik <hakancelik96@outlook.com>
2017-12-05 14:54:46 +0200

    change.py liste ismi değiştirme sorunu

--- cooggerapp/views/controls.py
+++ cooggerapp/views/controls.py
@@ -69,18 +69,17 @@ def change(request,content_id):
         queryset = Blog.objects.filter(id = content_id)
     else:
         queryset = Blog.objects.filter(username = request_user,id = content_id)
-    real_username = queryset[0].username # içeriği yazan kişinin kullanıcı ismi
-    real_user = User.objects.filter(username = real_username )[0]
     if not queryset.exists():
         ms.error(request,"Girmek istediğiniz sayfada yönetim iznine sahip değilsiniz !")
         return HttpResponseRedirect("/")
     queryset = queryset[0]
+    real_username = queryset.username # içeriği yazan kişinin kullanıcı ismi
     old_content_list = str(queryset.content_list)
     change_form = ContentForm(request.POST or None,instance=queryset)
     # post method
     if change_form.is_valid():
         content = change_form.save(commit=False)
-        content.username = real_user
+        content.username = real_username
         content_list = str(slugify(request.POST["content_list"]))
         content.content_list = content_list
         content.time = queryset.time
@@ -98,8 +97,9 @@ def change(request,content_id):
         content.tag = tags
         content.save()
         if content_list != old_content_list: # content_list değişmiş ise
+            real_user = User.objects.filter(username = real_username)[0]
             try:
-                content_list_save = ContentList.objects.filter(user = request_user,content_list = old_content_list)[0]
+                content_list_save = ContentList.objects.filter(user = real_user,content_list = old_content_list)[0]
                 content_list_save.content_count = F("content_count")-1 # eskisini bir azaltıyor
                 content_list_save.save()
                 try:
@@ -109,11 +109,11 @@ def change(request,content_id):
             except IndexError:
                 pass
             try:
-                content_list_ = ContentList.objects.filter(user = request_user,content_list = content_list)[0]
+                content_list_ = ContentList.objects.filter(user = real_user,content_list = content_list)[0]
                 content_list_.content_count = F("content_count")+1
                 content_list_.save()
             except:
-                ContentList(username = request_user,content_list = content_list,content_count = 1).save()
+                ContentList(user = real_user,content_list = content_list,content_count = 1).save()
         return HttpResponseRedirect("/@"+str(real_username)+"/"+content_list+"/"+url)
     # get method
     output = dict(
