3b393e2a9ce7946473d63e4458bb324a2ea9fef1
hakancelik96 <hakancelik96@outlook.com>
2017-12-22 16:37:06 +0300

    ignore

--- coogger/settings.py
+++ coogger/settings.py
@@ -1,8 +1,8 @@
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0o-ibh!$m!46+2y^9720!@pu(g*($hy1m0^89b%j8hrwr%k!$k'
-DEBUG = False
-ALLOWED_HOSTS = [".coogger.com"]
+DEBUG = True
+ALLOWED_HOSTS = ["*"]
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
