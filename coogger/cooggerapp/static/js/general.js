58322fff7bc193f50c1b5adffc9e58eccd8d1905
hakan çelik <hakancelik96@outlook.com>
2018-08-31 18:16:15 +0300

    desing header and right-left sections fixed

--- coogger/cooggerapp/static/js/general.js
+++ coogger/cooggerapp/static/js/general.js
@@ -2,6 +2,16 @@ $(document).ready(function() {
     $(".close-ms").click(function() {
         $(".main-messages").remove();
     })
+    $(".open_header_menu").click(function() {
+        $(".closed_header_menu").css({"display":"flex"});
+        $(".header_menu").toggle("fast", function(){
+          $(".header_menu").css({"display":"flex"});
+        });
+    })
+    $(".closed_header_menu").click(function() {
+        $(".closed_header_menu").css({"display":"none"});
+        $(".header_menu").toggle("fast", function() {});
+    })
     $(".b-edit-point").click(function() {
         var data_edit_id = this.getAttribute("data-edit-id");
         $(".data-edit-id-" + data_edit_id).toggle("fast");
@@ -9,7 +19,10 @@ $(document).ready(function() {
     $(".lists").click(function() {
         $("nav").toggle("fast");
     });
-    $(".fa-arrow-down").click(function() {
-        $(".open_header_menu").toggle("fast", function() {});
-    });
+    if($(window).width() < 800)
+    {
+        $("header").attr("gnrl", "br-1 bg-white");
+    } else {
+        $("header").attr("gnrl", "br-1 bg-white center");
+    }
 });
