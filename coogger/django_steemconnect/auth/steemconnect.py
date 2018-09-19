b7589cce6dae4709574cbdbc6b2606b2b29db4b6
hakan çelik <hakancelik96@outlook.com>
2018-09-19 18:45:39 +0300

    SteemConnectBackend account check

--- coogger/django_steemconnect/auth/steemconnect.py
+++ coogger/django_steemconnect/auth/steemconnect.py
@@ -1,13 +1,19 @@
+# django
 from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
+from django.http import Http404
 
 # models
 from cooggerapp.models import OtherInformationOfUsers
 
+# steem
+from steem.steem import Steem
 
 class SteemConnectBackend:
 
     def authenticate(self, request, username=None, password=None):
+        if Steem().get_account(username) is None:
+            raise Http404
         user, created = User.objects.get_or_create(username=username)
         if created:
             OtherInformationOfUsers(user=user).save()
