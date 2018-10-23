673df1c3b825c355a88ab9aeee0b08a5d7cec861
hakan çelik <hakancelik96@outlook.com>
2018-10-24 00:41:59 +0300

    api permission improved

--- coogger/cooggerapi/permissions.py
+++ coogger/cooggerapi/permissions.py
@@ -5,43 +5,38 @@ from django.http import Http404
 
 
 class ApiPermission(BasePermission):
-    message = 'You should write a access_token for allow api services.'
+    message = "Something went wrong !"
     my_safe_method = ["GET", "POST"]
 
     def has_permission(self, request, view):
-        if request.method in self.my_safe_method:
+        if request.user.is_superuser:
+            return True
+        try:
+            get_operation = request.data["operation"]
+            data_username = request.data["username"]
+            data_access_token = request.data["access_token"]
+            user = User.objects.filter(username=data_username)[0]
+            access_token = OtherInformationOfUsers.objects.filter(user=user)[0].access_token
+        except:
+            get_operation = None
+        if get_operation == "Filter":
+            if user.is_superuser and access_token == data_access_token:
+                return True
+            return False
+        elif get_operation == "Update_or_Get":
             try:
-                get_operation = request.data["operation"]
-                data_username = request.data["username"]
-                data_access_token = request.data["access_token"]
-                user = User.objects.filter(username=data_username)[0]
-                access_token = OtherInformationOfUsers.objects.filter(user=user)[0].access_token
-            except:
-                get_operation = None
-            if get_operation == "Filter":
-                if user.is_superuser and access_token == data_access_token:
+                username = request.parser_context["kwargs"]["username"]
+                if username == data_username and access_token == data_access_token:
                     return True
+            except KeyError:
                 return False
-            elif get_operation == "Update_or_Get":
-                try:
-                    username = request.parser_context["kwargs"]["username"]
-                except KeyError:
-                    username = request.parser_context["kwargs"]["pk"].replace("@", "")
-                if user.is_superuser and access_token == data_access_token:
+        elif get_operation == None:
+            try:
+                username = request.parser_context["kwargs"]["username"]
+                if request.user.username == username:
                     return True
-                elif username == data_username and access_token == data_access_token:
-                    return True
+            except KeyError:
                 return False
-            elif get_operation == None:
-                try:
-                    username = request.parser_context["kwargs"]["username"]
-                except KeyError:
-                    return False
-                if request.user.is_superuser:
-                    return True
-                elif request.user.username == username:
-                    return True
-        return False
 
     def has_object_permission(self, request, view, obj):
         return self.has_permission(request, view)
