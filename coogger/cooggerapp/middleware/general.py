bb8e18032d76ab9494a4b6943b7cd754c2a96323
hakan çelik <hakancelik96@outlook.com>
2018-11-11 17:21:28 +0300

    editor template bug fixed on create and update page, categories sort by len

--- coogger/cooggerapp/middleware/general.py
+++ coogger/cooggerapp/middleware/general.py
@@ -2,7 +2,7 @@ from django.utils.deprecation import MiddlewareMixin
 from django.urls import resolve
 
 # models.
-from cooggerapp.models import CategoryofDapp
+from cooggerapp.models import CategoryofDapp, Content
 from steemconnect_auth.models import Dapp
 
 # coices
@@ -11,7 +11,19 @@ from cooggerapp.choices import *
 class GeneralMiddleware(MiddlewareMixin):
 
     def process_request(self, request):
-        category_objects = CategoryofDapp.objects
-        category_filter = category_objects.filter(dapp=request.dapp_model)
-        request.categories = make_choices([category.category_name for category in category_filter])
+        if request.dapp_model.name == "coogger":
+            category_filter = CategoryofDapp.objects.all()
+        else:
+            category_filter = CategoryofDapp.objects.filter(dapp=request.dapp_model)
+        querysets_list = []
+        for category in category_filter:
+            querysets = Content.objects.filter(category = category.category_name, status="approved")
+            querysets_list.append(querysets)
+        categories = []
+        for contents in sorted(querysets_list, key=len, reverse=True):
+            try:
+                categories.append(contents[0].category)
+            except IndexError:
+                pass
+        request.categories = make_choices([category for category in categories])
         request.languages = make_choices(languages)
