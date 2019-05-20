2bce946d5a66df3fc2f27ead416e50a2e3cbaaa7
hakan çelik <hakancelik96@outlook.com>
2019-05-20 06:40:27 +0300

    user url bug fixed

--- core/cooggerapp/urls/users.py
+++ core/cooggerapp/urls/users.py
@@ -5,10 +5,9 @@ from django.urls import path
 from core.cooggerapp.views.users import (About, Home, Comment, Wallet, Activity)
 
 urlpatterns = [
-    # url(r'^upload/pp/$', users.Uploadpp.as_view(), name="user_upload_pp"),
-    path('about/@<username>/', About.as_view(), name="userabout"),
+    path('u/@<username>/about/', About.as_view(), name="userabout"),
     path('@<username>/', Home.as_view(), name="user"),
-    path('comment/@<username>', Comment.as_view(), name="comment"),
-    path('wallet/@<username>', Wallet.as_view(), name="wallet"),
-    path('activity/@<username>', Activity.as_view(), name="activity"),
+    path('u/@<username>/comment/', Comment.as_view(), name="comment"),
+    path('u/wallet/@<username>', Wallet.as_view(), name="wallet"),
+    path('u/@<username>/activity/', Activity.as_view(), name="activity"),
     ]
