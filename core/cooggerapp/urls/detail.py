ff58ff983283385288f9fc3c3df83351f3468ac6
hakan çelik <hakancelik96@outlook.com>
2019-05-21 16:06:35 +0300

    fixed urls bugs

--- core/cooggerapp/urls/detail.py
+++ core/cooggerapp/urls/detail.py
@@ -6,11 +6,7 @@ from django.shortcuts import redirect
 from core.cooggerapp.views.detail import Embed, Detail
 
 
-def detail_redirect(request, category, username, permlink):
-    return redirect(f"/@{username}/{permlink}")
-
 urlpatterns = [
     path('embed/@<username>/<permlink>/', Embed.as_view(), name="embed"),
-    path('<category>/@<username>/<permlink>/', detail_redirect, name="detail_redirect"),
     path('@<username>/<permlink>/', Detail.as_view(), name="detail"),
     ]
