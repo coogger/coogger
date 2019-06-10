2ba729a35acbb3f6f7e48af8f54438258d362f99
hakan çelik <hakancelik96@outlook.com>
2019-06-11 02:22:02 +0300

    post view unnecassary code removed

--- core/cooggerapp/views/post.py
+++ core/cooggerapp/views/post.py
@@ -14,6 +14,11 @@ from ..models import Content, Category, UTopic, Topic
 from ..forms import ContentForm, ReplyForm
 
 
+def redirect_utopic(request, utopic_permlink):
+    messages.warning(request, f"you need to create the {utopic_permlink} topic first.")
+    return redirect(reverse("create-utopic")+f"?name={utopic_permlink}")
+
+
 class Create(LoginRequiredMixin, View):
     template_name = "post/create.html"
     form_class = ContentForm
@@ -22,20 +27,12 @@ class Create(LoginRequiredMixin, View):
     def get(self, request, utopic_permlink, *args, **kwargs):
         initial, category = dict(), None
         if not UTopic.objects.filter(user=request.user, permlink=utopic_permlink).exists():
-            messages.warning(request, f"you need to create the {utopic_permlink} topic first.")
-            return redirect(reverse("create-utopic")+f"?name={utopic_permlink}")
+            return redirect_utopic(request, utopic_permlink)
         for key, value in request.GET.items():
             if key == "category":
                 category = Category.objects.get(name=value)
                 category_template = category.template
                 initial.__setitem__("category", category)
-            elif key == "topic":
-                try:
-                    utopic = UTopic.objects.filter(user=request.user, name=value)[0]
-                except IndexError:
-                    messages.warning(request, f"you need to create the {value} topic first.")
-                    return redirect(reverse("create-utopic")+f"?name={value}")
-                initial.__setitem__("topic", utopic)
             else:
                 initial.__setitem__(key, value)
         if category is None:
@@ -52,12 +49,20 @@ class Create(LoginRequiredMixin, View):
         if form.is_valid():
             form = form.save(commit=False)
             save = form.content_save(request, form, utopic_permlink)
-            return redirect(reverse("detail", kwargs=dict(username=str(form.user), permlink=form.permlink)))
+            return redirect(
+                reverse(
+                    "detail", 
+                    kwargs=dict(
+                        username=str(form.user), 
+                        permlink=form.permlink
+                    )
+                )
+            )
         return render(request, self.template_name, dict(form=form))
 
 
-class Change(LoginRequiredMixin, View):
-    template_name = "post/change.html"
+class Update(LoginRequiredMixin, View):
+    template_name = "post/update.html"
     form_class = ContentForm
     reply_form_class = ReplyForm
     model = Content
@@ -67,21 +72,24 @@ class Change(LoginRequiredMixin, View):
             utopic_permlink = request.GET.get("utopic_permlink", None)
             if utopic_permlink is not None and not UTopic.objects.filter(
                 user=request.user, permlink=utopic_permlink).exists():
-                messages.warning(request, f"you need to create the {utopic_permlink} topic first.")
-                return redirect(reverse("create-utopic")+f"?name={utopic_permlink}")
+                return redirect_utopic(request, utopic_permlink)
             queryset = self.model.objects.filter(user=request.user, permlink=permlink)
             if queryset.exists():
                 if queryset[0].reply is not None:
                     form_set = self.reply_form_class(instance=queryset[0])
                 else:
-                    form_set = self.form_class(instance=queryset[0], initial=dict(msg=f"Update {queryset[0].title.lower()}"))
+                    form_set = self.form_class(
+                        instance=queryset[0], 
+                        initial=dict(
+                            msg=f"Update {queryset[0].title.lower()}"
+                        )
+                    )
                 context = dict(
                     username=username,
                     permlink=permlink,
                     form=form_set,
                 )
                 return render(request, self.template_name, context)
-        raise Http404
 
     def post(self, request, username, permlink, *args, **kwargs):
         if request.user.username == username:
