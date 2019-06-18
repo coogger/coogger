bf86a61c64dc7470f7a3a1994643feefbf5c6de7
hakan çelik <hakancelik96@outlook.com>
2019-06-18 17:38:34 +0300

    admin improved use foreign key search

--- core/cooggerapp/admin.py
+++ core/cooggerapp/admin.py
@@ -1,10 +1,9 @@
-from django.contrib.admin import ModelAdmin, site
+from django.contrib import admin
 
 #models
 from core.cooggerapp.models import (
     Content, 
     SearchedWords,
-    ReportModel, 
     UserProfile, 
     Topic, 
     Commit, 
@@ -20,11 +19,18 @@ from core.cooggerapp.choices import *
 import datetime
 
 
-class ContentAdmin(ModelAdmin):
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Content)
+class ContentAdmin(admin.ModelAdmin):
     list_display = ["user","permlink", "mod", "reply"]
     list_display_links = list_display
     list_filter = ["status"]
-    search_fields = ["title", "body", "permlink"]
+    search_fields = ["user__username", "permlink", "title", "body"]
+    prepopulated_fields = {"permlink": ("title",)}
     fields = (
         ("user"),
         ("category", "language", "utopic"),
@@ -41,19 +47,23 @@ class ContentAdmin(ModelAdmin):
         super().save_model(request, obj, form, change)
 
 
-class SearchedWordsAdmin(ModelAdmin):
+@admin.register(SearchedWords)
+class SearchedWordsAdmin(admin.ModelAdmin):
     list_display = ["word","hmany"]
     list_display_links = list_display
     search_fields = list_display
 
 
-class UserProfileAdmin(ModelAdmin):
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
     list_display = ["user"]
     list_display_links = list_display
     filter_horizontal = ("address", )
+    search_fields = ["user__username"]
     
 
-class TopicAdmin(ModelAdmin):
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
     list_ = ["name", "tags", "editable"]
     list_display = list_
     list_display_links = list_
@@ -68,17 +78,19 @@ class TopicAdmin(ModelAdmin):
     )
 
 
-class CommitAdmin(ModelAdmin):
+@admin.register(Commit)
+class CommitAdmin(admin.ModelAdmin):
     list_display = ["content", "utopic"]
     list_display_links = ["content", "utopic"]
     list_filter = ["created"]
-    search_fields = ["msg", "body"]
+    search_fields = ["user__username", "utopic__name", "msg"]
 
 
-class UtopicAdmin(ModelAdmin):
+@admin.register(UTopic)
+class UtopicAdmin(admin.ModelAdmin):
     list_display = ["user", "name"]
     list_display_links = ["user", "name"]
-    search_fields = ["name"]
+    search_fields = ["user__username", "name"]
     fields = (
         ("user"),
         ("name"),
@@ -95,19 +107,9 @@ class UtopicAdmin(ModelAdmin):
     )
 
 
-class IssueAdmin(ModelAdmin):
+@admin.register(Issue)
+class IssueAdmin(admin.ModelAdmin):
     list_display = ["user", "utopic", "title"]
     list_display_links = list_display
-    search_fields = ["title", "body"]
+    search_fields = ["user__username", "utopic__name", "title"]
     list_filter = ["status", "created"]
-
-
-site.register(Content, ContentAdmin)
-site.register(Commit, CommitAdmin)
-site.register(UTopic, UtopicAdmin)
-site.register(Category)
-site.register(SearchedWords, SearchedWordsAdmin)
-site.register(ReportModel)
-site.register(UserProfile, UserProfileAdmin)
-site.register(Topic, TopicAdmin)
-site.register(Issue, IssueAdmin)
