15b2fe237ba9fdae3d0404d2ebb5307bc7b755b9
Yunus Emre <yunusemregeldegul@gmail.com>
2019-09-05 21:54:15 +0300

    unused 'Redirect'
    from django.contrib.redirects.models import Redirect

--- core/cooggerapp/signals/content.py
+++ core/cooggerapp/signals/content.py
@@ -1,4 +1,3 @@
-from django.contrib.redirects.models import Redirect
 from django.db.models import F
 from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
